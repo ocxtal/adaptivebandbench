f51ed84dbe44b9a5e64b371b6697f37f67442c0e
Hajime Suzuki <yline5050hs@gmail.com>
2015-11-30 12:07:43 +0900

    merge bench.py from master

--- bench.py
+++ bench.py
@@ -4,7 +4,7 @@
 import subprocess
 
 pbsim_path = '/home/suzukihajime/src/pbsim-1.0.3/src/'
-ref_path = '/home/suzukihajime/oni/work/resource/NC_000913.fna'
+ref_path = '/home/suzukihajime/oni/work/NC_000913.fna'
 align_path = './a.out'
 # ./pbsim --data-type CLR --length-min 900 --length-max 1100 --accuracy-min 0.84 --accuracy-max 0.86 --model_qc ../data/model_qc_clr --length-mean 1000 --length-sd 100 --accuracy-mean 0.85 --accuracy-sd 0.01 ~/docs/oni/work/NC_000913.fna
 
@@ -40,14 +40,16 @@ def parse_maf(maf_path):
 
 			yield((ref, read))
 
-def align(align_paths, algorithms, ref, read):
+def align(align_paths, algorithms, ref, read, m, x, gi, ge):
 	return([[int(subprocess.check_output([
-		path, alg, ref, read, '2', '-3', '-5', '-1', '30']).split()[0])
+		path, alg, ref, read, str(m), str(x), str(gi), str(ge), '30']).split()[0])
 			for path in align_paths]
 			for alg in algorithms])
 
 
-def evaluate(pbsim_path, ref_path, algorithm, length, error_rate):
+def evaluate(pbsim_path, ref_path, m, x, gi, ge, length, error_rate):
+
+	# print(m, x, gi, ge, length, error_rate)
 
 	pbsim(pbsim_path, ref_path, length, 20, error_rate)
 	pairs = parse_maf('./sd_0001.maf')
@@ -60,13 +62,15 @@ def evaluate(pbsim_path, ref_path, algorithm, length, error_rate):
 
 		ref = pair[0] + ''.join(['A' for i in range(100)])
 		read = pair[1] + ''.join(['T' for i in range(100)])
+
 		# print(ref, read)
-		scores = align(['./blast', './ddiag'], ['affine', 'linear'], ref, read)
+
+		scores = align(['./blast', './ddiag'], ['linear', 'affine'], ref, read, m, x, gi, ge)
 		succ = [1 if score[0] == score[1] else 0 for score in scores]
-		# print(scores, succ, acc)
+		print(scores, succ, acc)
 		acc = [sum(x) for x in zip(succ, acc)]
 		tot = tot + 1
-		if tot == 1000:
+		if tot == 10:
 			break
 		
 
@@ -77,11 +81,32 @@ if __name__ == '__main__':
 
 	lengths = [100, 200, 500, 1000, 2000, 5000, 10000]
 	error_rates = [0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
+	xs = [-i for i in range(3, 9)]		# -1..-3
+	gs = [-i for i in range(3, 15)]		# -1..-5
 
-	arg_list = [(len, err) for len in lengths for err in error_rates]
+	results = [[[[evaluate(pbsim_path, ref_path, 3, x, g, -3, l, e)
+		for e in error_rates]
+		for l in lengths]
+		for g in gs]
+		for x in xs]
 
-	for a in arg_list:
-		r = evaluate(pbsim_path, ref_path, 'affine', a[0], a[1])
-		print("{}, {}, {}, {}".format(a[0], a[1], r[0], r[1]))
+	from numpy import *
+	res_arr = array(results)
+
+	res_linear = res_arr[
+		0:len(xs),
+		0:len(gs),
+		0:len(lengths),
+		0:len(error_rates),
+		0]
+	res_affine = res_arr[
+		0:len(xs),
+		0:len(gs),
+		0:len(lengths),
+		0:len(error_rates),
+		1]
+
+	print(res_linear.tolist())
+	print(res_affine.tolist())
 
 
