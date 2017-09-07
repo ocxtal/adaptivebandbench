7fea2595953d7c00799065eec767073e95f64975
Hajime Suzuki <yline5050hs@gmail.com>
2017-09-08 08:35:58 +0900

    add bandwidth sweep in bench

--- main.cc
+++ main.cc
@@ -20,15 +20,11 @@
 #define X 					( 1 )
 #define GI 					( 1 )
 #define GE 					( 1 )
-#if defined(BW) && BW == 32
-#  define XDROP				( 100 )
-#else
-#  define XDROP				( 100 )
-#endif
+#define XDROP				( 70 )			// equal to the default of blastn (X = 100 (bit)) w/ (M, X, Gi, Ge) = (1, -1, 2, 1)
 
 // #define OMIT_SCORE			1
 #define PARASAIL_SCORE		1
-#define RECALL_THRESH		1
+#define RECALL_THRESH		0.99
 // #define DEBUG_PATH
 // #define DEBUG_BLAST			1
 
@@ -66,23 +62,52 @@ int simdblast_affine(
 	int8_t *score_matrix, int8_t gi, int8_t ge, int16_t xt);
 	// int8_t m, int8_t x, int8_t gi, int8_t ge, int16_t xt);
 
-
-int ddiag_linear(
-	void *work,
-	char const *a,
-	uint64_t alen,
-	char const *b,
-	uint64_t blen,
-	int8_t *score_matrix, int8_t ge, int16_t xt);
-	// int8_t m, int8_t x, int8_t gi, int8_t ge, int16_t xt);
-int ddiag_affine(
-	void *work,
-	char const *a,
-	uint64_t alen,
-	char const *b,
-	uint64_t blen,
-	int8_t *score_matrix, int8_t gi, int8_t ge, int16_t xt);
-	// int8_t m, int8_t x, int8_t gi, int8_t ge, int16_t xt);
+#define cat_name(x, y)			x##_##y
+#define export_name(x, y)		cat_name(x, y)
+#define ddiag_decl(_bw) \
+	int export_name(ddiag_linear, _bw)( \
+		void *work, \
+		char const *a, \
+		uint64_t alen, \
+		char const *b, \
+		uint64_t blen, \
+		int8_t *score_matrix, int8_t ge, int16_t xt); \
+	int export_name(ddiag_affine, _bw)( \
+		void *work, \
+		char const *a, \
+		uint64_t alen, \
+		char const *b, \
+		uint64_t blen, \
+		int8_t *score_matrix, int8_t gi, int8_t ge, int16_t xt);
+ddiag_decl(32)
+ddiag_decl(40)
+ddiag_decl(48)
+ddiag_decl(56)
+ddiag_decl(64)
+ddiag_decl(72)
+ddiag_decl(80)
+ddiag_decl(88)
+ddiag_decl(96)
+ddiag_decl(104)
+ddiag_decl(112)
+ddiag_decl(120)
+ddiag_decl(128)
+ddiag_decl(136)
+ddiag_decl(144)
+ddiag_decl(152)
+ddiag_decl(160)
+ddiag_decl(168)
+ddiag_decl(176)
+ddiag_decl(184)
+ddiag_decl(192)
+ddiag_decl(200)
+ddiag_decl(208)
+ddiag_decl(216)
+ddiag_decl(224)
+ddiag_decl(232)
+ddiag_decl(240)
+ddiag_decl(248)
+ddiag_decl(256)
 
 /* wrapper of Myers' wavefront algorithm */
 extern "C" {
@@ -102,7 +127,7 @@ struct wavefront_work_s {
 	Path apath, bpath;
 };
 
-struct wavefront_work_s *wavefront_init_work(void)
+struct wavefront_work_s *wavefront_init_work(double id)
 {
 	struct wavefront_work_s *work = (struct wavefront_work_s *)malloc(sizeof(struct wavefront_work_s));
 
@@ -112,7 +137,7 @@ struct wavefront_work_s *wavefront_init_work(void)
 
 
 	float freq[4] = { 0.25, 0.25, 0.25, 0.25 };
-	Align_Spec *s = New_Align_Spec(0.6, 100, freq);
+	Align_Spec *s = New_Align_Spec(id, 100, freq);
 
 
 	work->w = w;
@@ -270,10 +295,14 @@ int print_msg(int flag, char const *fmt, ...)
 	return(r);
 }
 
-int print_bench(int flag, char const *name, int64_t l, int64_t a, int64_t sl, int64_t sa)
+int print_bench(int flag, char const *name, int suffix, int64_t l, int64_t a, int64_t sl, int64_t sa)
 {
 	if(flag == 0) {
-		return(printf("%s\t%ld\t%ld\t%ld\t%ld\n", name, l / 1000, a / 1000, sl, sa));
+		if(suffix == 0) {
+			return(printf("%s\t%ld\t%ld\t%ld\t%ld\n", name, l / 1000, a / 1000, sl, sa));
+		} else {
+			return(printf("%s.%d\t%ld\t%ld\t%ld\t%ld\n", name, suffix, l / 1000, a / 1000, sl, sa));
+		}
 	} else if(flag == 1) {
 		return(printf("%ld\t%ld\t", l / 1000, a / 1000));
 	} else if(flag == 2) {
@@ -309,7 +338,7 @@ int main(int argc, char *argv[])
 	srand(s);
 
 	if(flag == 0) {
-		print_msg(flag, "%lu\n", s);
+		print_msg(flag, "seed:%lu\tm: %d\tx: %d\tgi: %d\tge: %d\txdrop: %d\n", s, m, x, gi, ge, xt);
 	}
 
 	/* malloc work */
@@ -345,7 +374,7 @@ int main(int argc, char *argv[])
 			sba += blast_affine(work, a, strlen(a), b, strlen(b), score_matrix, gi, ge, xt);
 		}
 		bench_end(ba);
-		print_bench(flag, "blast", bench_get(bl), bench_get(ba), sbl, sba);
+		print_bench(flag, "blast", xt, bench_get(bl), bench_get(ba), sbl, sba);
 
 		/* simdblast */
 		bench_init(sl);
@@ -360,7 +389,7 @@ int main(int argc, char *argv[])
 			ssa += simdblast_affine(work, a, strlen(a), b, strlen(b), score_matrix, gi, ge, xt);
 		}
 		bench_end(sa);
-		print_bench(flag, "simdblast", bench_get(sl), bench_get(sa), ssl, ssa);
+		print_bench(flag, "simdblast", xt, bench_get(sl), bench_get(sa), ssl, ssa);
 
 
 		/* adaptive banded */
@@ -368,22 +397,22 @@ int main(int argc, char *argv[])
 		bench_init(dda);
 		bench_start(ddl);
 		for(i = 0; i < cnt; i++) {
-			sddl += ddiag_linear(work, a, strlen(a), b, strlen(b), score_matrix, ge, xt);
+			sddl += ddiag_linear_32(work, a, strlen(a), b, strlen(b), score_matrix, ge, xt);
 		}
 		bench_end(ddl);
 		bench_start(dda);
 		for(i = 0; i < cnt; i++) {
-			sdda += ddiag_affine(work, a, strlen(a), b, strlen(b), score_matrix, gi, ge, xt);
+			sdda += ddiag_affine_32(work, a, strlen(a), b, strlen(b), score_matrix, gi, ge, xt);
 		}
 		bench_end(dda);
-		print_bench(flag, "aband", bench_get(ddl), bench_get(dda), sddl, sdda);
+		print_bench(flag, "aband", 32, bench_get(ddl), bench_get(dda), sddl, sdda);
 
 		/* wavefront */
 		bench_init(wl);
 		if(len >= 1000) {
 			/* wavefront algorithm fails to align sequences shorter than 1000 bases */
 
-			struct wavefront_work_s *wwork = wavefront_init_work();
+			struct wavefront_work_s *wwork = wavefront_init_work(0.6);
 
 			bench_start(wl);
 			for(i = 0; i < cnt; i++) {
@@ -391,8 +420,21 @@ int main(int argc, char *argv[])
 			}
 			bench_end(wl);
 		}
-		print_bench(flag, "wavefront", bench_get(wl), bench_get(wl), 0, 0);
+		print_bench(flag, "wavefront-0.6", 0, bench_get(wl), bench_get(wl), 0, 0);
 
+		bench_init(wl);
+		if(len >= 1000) {
+			/* wavefront algorithm fails to align sequences shorter than 1000 bases */
+			// default: an average correlation rate of -e (default 70%)
+			struct wavefront_work_s *wwork = wavefront_init_work(0.7);
+
+			bench_start(wl);
+			for(i = 0; i < cnt; i++) {
+				wavefront(wwork, a, strlen(a), b, strlen(b));
+			}
+			bench_end(wl);
+		}
+		print_bench(flag, "wavefront-0.7", 0, bench_get(wl), bench_get(wl), 0, 0);
 
 		if(flag != 0) {
 			printf("\n");
@@ -473,82 +515,138 @@ int main(int argc, char *argv[])
 		#endif
 
 		/* blast */
-		bench_init(bl);
-		bench_init(ba);
-		bench_start(ba);
-		for(i = 0; i < kv_size(seq) / 2; i++) {
-			uint32_t s = blast_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xt);
-			sba += s >= RECALL_THRESH * kv_at(ascore, i);
+		for(int xdrop = 40; xdrop <= 100; xdrop += 5) {
+			if(flag > 0 && xdrop != 70) { continue; }
 
-			#if defined(DEBUG_PATH) && defined(DEBUG_BLAST)
-			if(s < RECALL_THRESH * kv_at(ascore, i)) {
-				sw_result_t a = sw_affine(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge);
-				print_alignment(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), a.path);
-				free(a.path);
+			bench_init(bl);
+			bench_init(ba);
+			bench_start(ba);
+			sba = 0;
+			for(i = 0; i < kv_size(seq) / 2; i++) {
+				uint32_t s = blast_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xdrop);
+				sba += s >= RECALL_THRESH * kv_at(ascore, i);
+
+				#if defined(DEBUG_PATH) && defined(DEBUG_BLAST)
+				if(s < RECALL_THRESH * kv_at(ascore, i)) {
+					sw_result_t a = sw_affine(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge);
+					print_alignment(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), a.path);
+					free(a.path);
+				}
+				#endif
 			}
-			#endif
+			bench_end(ba);
+			print_bench(flag, "blast", xdrop, bench_get(bl), bench_get(ba), sbl, sba);
 		}
-		bench_end(ba);
-		print_bench(flag, "blast", bench_get(bl), bench_get(ba), sbl, sba);
 
 		#ifndef DEBUG_PATH
 			/* simdblast */
-			bench_init(sl);
-			bench_init(sa);
-			bench_start(sa);
-			for(i = 0; i < kv_size(seq) / 2; i++) {
-				uint32_t s = simdblast_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xt);
-				ssa += s >= RECALL_THRESH * kv_at(ascore, i);
+			for(int xdrop = 40; xdrop <= 100; xdrop += 5) {
+				if(flag > 0 && xdrop != 70) { continue; }
+
+				bench_init(sl);
+				bench_init(sa);
+				bench_start(sa);
+				ssa = 0;
+				for(i = 0; i < kv_size(seq) / 2; i++) {
+					uint32_t s = simdblast_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xdrop);
+					ssa += s >= RECALL_THRESH * kv_at(ascore, i);
+				}
+				bench_end(sa);
+				print_bench(flag, "simdblast", xdrop, bench_get(sl), bench_get(sa), ssl, ssa);
 			}
-			bench_end(sa);
-			print_bench(flag, "simdblast", bench_get(sl), bench_get(sa), ssl, ssa);
 		#endif
 
 		/* adaptive banded */
-		bench_init(ddl);
-		bench_init(dda);
-		bench_start(dda);
-		for(i = 0; i < kv_size(seq) / 2; i++) {
-			uint32_t s = ddiag_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xt);
-			sdda += s >= RECALL_THRESH * kv_at(ascore, i);
+		int (*ddiag_affine_table[])(
+			void *work,
+			char const *a,
+			uint64_t alen,
+			char const *b,
+			uint64_t blen,
+			int8_t score_matrix[16], int8_t gi, int8_t ge, int16_t xt) = {
 
-			#if defined(DEBUG_PATH) && !defined(DEBUG_BLAST)
-			if(s < RECALL_THRESH * kv_at(ascore, i)) {
-				sw_result_t a = sw_affine(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge);
-				print_alignment(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), a.path);
-				free(a.path);
-			}
-			#endif
-		}
-		bench_end(dda);
-		print_bench(flag, "aband", bench_get(ddl), bench_get(dda), sddl, sdda);
+			ddiag_affine_32,
+			ddiag_affine_40,
+			ddiag_affine_48,
+			ddiag_affine_56,
+			ddiag_affine_64,
+			ddiag_affine_72,
+			ddiag_affine_80,
+			ddiag_affine_88,
+			ddiag_affine_96,
+			ddiag_affine_104,
+			ddiag_affine_112,
+			ddiag_affine_120,
+			ddiag_affine_128,
+			ddiag_affine_136,
+			ddiag_affine_144,
+			ddiag_affine_152,
+			ddiag_affine_160,
+			ddiag_affine_168,
+			ddiag_affine_176,
+			ddiag_affine_184,
+			ddiag_affine_192,
+			ddiag_affine_200,
+			ddiag_affine_208,
+			ddiag_affine_216,
+			ddiag_affine_224,
+			ddiag_affine_232,
+			ddiag_affine_240,
+			ddiag_affine_248,
+			ddiag_affine_256
+		};
 
-		int xtable[9] = { 20, 40, 60, 80, 100, 120, 140, 160, 180 };
-		for(uint64_t k = 0; k < 9; k++) {
+		for(int j = 0; j < sizeof(ddiag_affine_table) / sizeof(void *); j++) {
+			if(flag > 0 && j != 8) { continue; }
+
+			bench_init(ddl);
+			bench_init(dda);
+			bench_start(dda);
 			sdda = 0;
 			for(i = 0; i < kv_size(seq) / 2; i++) {
-				uint32_t s = ddiag_affine(work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xtable[k]);
+				uint32_t s = ddiag_affine_table[j](work, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge, xt);
 				sdda += s >= RECALL_THRESH * kv_at(ascore, i);
+
+				#if defined(DEBUG_PATH) && !defined(DEBUG_BLAST)
+				if(s < RECALL_THRESH * kv_at(ascore, i)) {
+					sw_result_t a = sw_affine(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), score_matrix, gi, ge);
+					print_alignment(kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1), a.path);
+					free(a.path);
+				}
+				#endif
 			}
-			printf("%llu\t%lld\n", xtable[k], sdda);
+			bench_end(dda);
+			print_bench(flag, "aband", j * 8 + 32, bench_get(ddl), bench_get(dda), sddl, sdda);
 		}
-
-
 
 		#ifndef DEBUG_PATH
 			/* wavefront */
 			bench_init(wl);
-			struct wavefront_work_s *wwork = wavefront_init_work();
-			
+			struct wavefront_work_s *wwork = wavefront_init_work(0.7);
+			if(max_len >= 1) {
+				bench_start(wl);
+				for(i = 0; i < kv_size(seq) / 2; i++) {
+					uint32_t l = wavefront(wwork, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1));
+					swl += l >= RECALL_THRESH * (kv_at(len, i * 2) + kv_at(len, i * 2 + 1) - 256);		// 128 bp margins for the both sequences
+				}
+				bench_end(wl);
+			}
+			wavefront_clean_work(wwork);
+			print_bench(flag, "wavefront-0.7", 0, bench_get(bl), bench_get(wl), 0, swl);
+
+			/* wavefront */
+			bench_init(wl);
+			wwork = wavefront_init_work(0.6);
 			if(max_len >= 100) {
 				bench_start(wl);
 				for(i = 0; i < kv_size(seq) / 2; i++) {
 					uint32_t l = wavefront(wwork, kv_at(seq, i * 2), kv_at(len, i * 2), kv_at(seq, i * 2 + 1), kv_at(len, i * 2 + 1));
-					swl += l >= RECALL_THRESH * (kv_at(len, i * 2) + kv_at(len, i * 2 + 1));
+					swl += l >= RECALL_THRESH * (kv_at(len, i * 2) + kv_at(len, i * 2 + 1) - 256);		// 128 bp margins for the both sequences
 				}
 				bench_end(wl);
 			}
-			print_bench(flag, "wavefront", bench_get(bl), bench_get(wl), 0, swl);
+			wavefront_clean_work(wwork);
+			print_bench(flag, "wavefront-0.6", 0, bench_get(bl), bench_get(wl), 0, swl);
 
 			/* parasail */
 			parasail_matrix_t *matrix = parasail_matrix_create("ACGT", M, -X);
@@ -561,7 +659,7 @@ int main(int argc, char *argv[])
 				spa += r->score >= RECALL_THRESH * kv_at(ascore, i);
 				parasail_result_free(r);
 			}
-			print_bench(flag, "parasail", bench_get(bl), bench_get(pa), 0, spa);
+			print_bench(flag, "parasail", 0, bench_get(bl), bench_get(pa), 0, spa);
 
 
 			/* SSW library */
@@ -584,7 +682,7 @@ int main(int argc, char *argv[])
 				align_destroy(r);
 				init_destroy(sp);
 			}
-			print_bench(flag, "farrar", bench_get(bl), bench_get(fa), 0, sfa);
+			print_bench(flag, "farrar", 0, bench_get(bl), bench_get(fa), 0, sfa);
 		#endif
 
 		if(flag != 0) {
--- makefile
+++ makefile
@@ -4,9 +4,10 @@ CXX=g++
 CFLAGS=-Wall -std=c99 -O3 -msse4.1 -fopenmp
 CXXFLAGS=-Wall -std=gnu++11 -O3 -msse4.1 -fopenmp
 
-BENCH_SRCS=main.cc aband.cc blast.cc simdblast.cc
+BENCH_SRCS=main.cc blast.cc simdblast.cc
 # BENCH_SRCS=main.cc alinear.cc aaffine.cc blast.cc simdblast.cc
 BENCH_MODULES=wave/DB.o wave/QV.o wave/align.o ssw.o parasail/cpuid.o parasail/io.o parasail/matrix_lookup.o parasail/memory.o parasail/memory_sse.o parasail/time.o sg_striped_sse41_128_16.o full.o
+ABAND_MODULES=$(shell seq -f'aband.%g.o ' 32 8 256)
 
 all: recall bench
 
@@ -15,9 +16,7 @@ recall:
 	for f in aband blast simdblast; do for bw in 8 16 24 32 40 48 56 64; do $(CXX) $(CXXFLAGS) -DMAIN -DBW=$$bw $$f.cc -o bin/$$f-$$bw; done; done
 	for bw in 8 16 24 32 40 48 56 64; do $(CC) $(CFLAGS) -DMAIN -DBW=$$bw full.c -o bin/full-$$bw; done
 
-
-
-bench.modules:
+$(BENCH_MODULES):
 	$(CC) $(CFLAGS) -c -o wave/DB.o wave/DB.c
 	$(CC) $(CFLAGS) -c -o wave/QV.o wave/QV.c
 	$(CC) $(CFLAGS) -c -o wave/align.o wave/align.c
@@ -31,29 +30,11 @@ bench.modules:
 	$(CC) $(CFLAGS) -c -o sg_striped_sse41_128_16.o -I. sg_striped_sse41_128_16.c
 	$(CC) $(CFLAGS) -c -o full.o full.c
 
-bench: bench.32 bench.64 bench.aband.32 bench.aband.64 bench.blast.32 bench.blast.64
+$(ABAND_MODULES):
+	$(CXX) $(CXXFLAGS) -c -o $@ -DBW=`echo $@ | cut -d'.' -f2` aband.cc
 
-bench.32: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.32 -DBW=32 -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
+bench: $(BENCH_MODULES) $(ABAND_MODULES)
+	$(CXX) $(CXXFLAGS) -o bin/bench -DBENCH $(BENCH_SRCS) $(BENCH_MODULES) $(ABAND_MODULES)
 
-bench.64: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.64 -DBW=64 -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.96: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.96 -DBW=96 -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.128: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.128 -DBW=128 -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.aband.32: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.aband.32 -DBW=32 -DDEBUG_PATH -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.aband.64: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.blast.64 -DBW=64 -DDEBUG_PATH -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.blast.32: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.blast.32 -DBW=32 -DDEBUG_PATH -DDEBUG_BLAST -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
-bench.blast.64: bench.modules
-	$(CXX) $(CXXFLAGS) -o bin/bench.blast.64 -DBW=64 -DDEBUG_PATH -DDEBUG_BLAST -DBENCH $(BENCH_SRCS) $(BENCH_MODULES)
-
+clean:
+	rm *.o bin/*
