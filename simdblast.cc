5c0f925b6acc3c281f9a412c713059aca45d4f4a
Hajime Suzuki <yline5050hs@gmail.com>
2018-01-14 16:00:21 +0900

    re-impl simdblast

--- simdblast.cc
+++ simdblast.cc
@@ -1,4 +1,4 @@
-#define DEBUG
+
 /**
  * @file simdblast.cc
  *
@@ -35,7 +35,7 @@ simdblast_affine(
 	debug("%s, %s", a, b);
 
 	uint64_t i, a_size, first_a_index, last_a_index, a_index, b_index;
-	vec const xtv(xt), zv, ofsv(OFS);
+	vec const xtv(xt), zv, ofsv(OFS), smv((uint16_t const *)score_matrix);
 	vec const giv(-gi), gev(-ge), gev2(-2*ge), gev4(-4*ge), gev8(-8*ge);
 	int16_t const acc_ge[vec::LEN] __attribute__(( aligned(16) )) = {
 		(int16_t)(0),
@@ -47,130 +47,126 @@ simdblast_affine(
 		(int16_t)(-6*ge),
 		(int16_t)(-7*ge)
 	};
-	vec acc_gev; acc_gev.load(acc_ge);
+	vec acc_gev((uint16_t const *)acc_ge);
 
 	int8_t sc_min = extract_min_score(score_matrix);
 
-	struct _dp {
-		int16_t best[vec::LEN];
-		int16_t best_gap[vec::LEN];
-	};
-	struct _dp *ptr = (struct _dp *)work, *prev = (struct _dp *)work + roundup(alen, vec::LEN) / vec::LEN;
 
+
+	uint64_t vblen = roundup(blen, vec::LEN) / vec::LEN;
+	uint64_t first_b_index = 0, last_b_index = vblen;		/* [first_b_index, last_b_index) */
+	uint64_t amax = 0, bmax = 0;
+
+	struct _dp {
+		uint16_t s[vec::LEN], e[vec::LEN], f[vec::LEN];
+	};
+	struct _dp *ptr = (struct _dp *)work + sizeof(maxpos_t), *prev;
 	debug("%llu, %llu, %llu, %llu, %lu", alen, roundup((alen + 1), vec::LEN), roundup((alen + 1), vec::LEN) / vec::LEN + 1, blen, sizeof(struct _dp));
 
-	/* init top row */
-	vec init_v = ofsv - (giv<<1) - acc_gev;
-	init_v.store(&ptr[0].best);
-	(init_v - giv).store(&ptr[0].best_gap);
-	init_v = ofsv - giv - gev8 - acc_gev;
-	for(i = 1; i < roundup(alen + 1, vec::LEN) / vec::LEN; i++) {
-		if((ofsv - init_v > xtv) == 0xffff) { break; }
-		init_v.store(&ptr[i].best);
-		(init_v - giv).store(&ptr[i].best_gap);
-		init_v -= gev8;
+	/* init the top row */
+	vec init_pv = ofsv - giv - gev - acc_gev, init_ev = zv;
+	init_pv.print("init_pv");
+	init_pv.store(ptr[0].s);
+	init_ev.store(ptr[0].e);
+	init_pv.store(ptr[0].f);
+	for(uint64_t i = 0; i < vblen - 1; i++) {
+		init_pv -= gev8;
+		if((init_pv < ofsv - xtv) == 0xffff) { last_b_index = i + 1; }
+		init_pv.print("init_pv");
+		init_pv.store(ptr[i + 1].s);
+		init_ev.store(ptr[i + 1].e);
+		init_pv.store(ptr[i + 1].f);
 	}
 
-	debug("%llu", i);
+	#define _gap(_i)		( ((_i) > 0 ? gi : 0) + (_i) * ge )
+	vec max(OFS);
+	for(uint64_t a_index = 0; a_index < alen; a_index++) {
+		debug("a_index(%llu), ch(%c), b_range(%llu, %llu)", a_index, a[a_index], first_b_index, last_b_index);
 
-	a_size = last_a_index = i;
-	first_a_index = 0;
-	vec best_score_v = ofsv;
-	vec next_score_v;
-	vec smv; smv.load(score_matrix);
+		prev = ptr; ptr += last_b_index + 1 - first_b_index;
+		char_vec av(encode_a(a[a_index]));
 
-	for(b_index = 1; b_index <= blen; b_index++) {
-		char_vec bv(encode_b(b[b_index-1]));
+		#define _update_vector(_i) { \
+			char_vec bv; bv.load_encode_b(&b[(_i) * vec::LEN], blen - (_i) * vec::LEN); \
+			vec th(prev[_i].s), te(prev[_i].e); \
+			/* calc tentative s and e */ \
+			pe = vec::max(te, th - giv) - gev; \
+			pv = vec::max(th.dsl(ch) + smv.shuffle(av | bv), pe); ch = th; \
+			av.print("av"); bv.print("bv"); smv.shuffle(av | bv).print("score"); \
+			/* calc f from the tentative s */ \
+			pf = vec::max(pv - giv, (pf>>7) - gev); \
+			pf = vec::max(pf, (pf<<1) - gev); \
+			pf = vec::max(pf, (pf<<2) - gev2); \
+			pf = vec::max(pf, (pf<<4) - gev4); \
+			/* fixup s */ \
+			pv = vec::max(pv, pf); \
+			/* update max */ \
+			mv = vec::max(mv, pv); \
+			pv.print("pv"); pe.print("pe"); pf.print("pf"); \
+		}
 
-		debug("%llu", last_a_index + 1 - first_a_index);
-		prev = ptr; ptr += (last_a_index + 1 - first_a_index);
-		last_a_index = first_a_index;
+		vec ch(OFS + _gap(a_index)), pv, pe, pf, mv(max[0]);
+/*		if(first_b_index > 0) {
+			ch.load(prev[first_b_index - 1].s); ch.print("initial ch");
+		}*/
 
-		vec score_v = zv;
-		vec score_gap_row_v = zv;
-		vec prev_pv = giv + gev4;
-		char_vec prev_av((uint64_t)0);
-		vec best_temp_v = best_score_v;
+		while(1) {
+			_update_vector(first_b_index);
+			if((pv < max - xt) != 0xffff) { break; }
+			first_b_index++; ptr--;
+		}
 
-		debug("loop b(%llu), first_a_index(%llu), a_size(%llu)", b_index, first_a_index, a_size);
-		for(a_index = first_a_index; a_index < a_size; a_index++) {
+		pv.store(ptr[first_b_index].s);
+		pe.store(ptr[first_b_index].e);
+		pf.store(ptr[first_b_index].f);
 
-			/* load prev vector */
-			vec pv; pv.load(&prev[a_index].best);
-			vec pf; pf.load(&prev[a_index].best_gap);
-			pv.print("pv");
+		debug("ptr(%p), b_range(%llu, %llu)", ptr, first_b_index, last_b_index);
+		uint64_t next_last_b_index = last_b_index;
+		for(uint64_t b_index = first_b_index + 1; b_index < last_b_index; b_index++) {
+			_update_vector(b_index);
+			if((pv < max - xt) != 0xffff) { next_last_b_index = b_index + 1; }
 
-			/* calc f */
-			vec score_gap_col_v = vec::max(pv - giv, pf) - gev;
+			pv.store(ptr[b_index].s);
+			pe.store(ptr[b_index].e);
+			pf.store(ptr[b_index].f);
+		}
+		last_b_index = next_last_b_index;
+		debug("updated b_range(%llu, %llu)", first_b_index, last_b_index);
 
-			/* calc e */
-			score_gap_row_v = (vec::max(score_v - giv, score_gap_row_v) - gev)>>7;
+		if(last_b_index < vblen) {
+			char_vec bv; bv.load_encode_b(&b[last_b_index * vec::LEN], blen - last_b_index * vec::LEN);
+			vec d = gev8.dsl(ch) + smv.shuffle(av | bv);
+			mv = vec::max(mv, d);
 
-			/* calc d */
-			score_v = pv.dsl(prev_pv);
-			char_vec av; av.load_encode_a(&a[a_index * vec::LEN], a_size - a_index * vec::LEN);
-			score_v += smv.shuffle(av.dsl(prev_av) | bv);
-			score_v = vec::max(score_v, score_gap_col_v);
+			pf = vec::max(d - giv, (pf>>7) - gev);
+			pf.set(pf[0]); pf -= acc_gev;
+			pv = vec::max(d, pf);
+			while(last_b_index < vblen) {
+				if((pv < max - xt) == 0xffff) { break; }
 
-			/* chain e */
-			score_v = vec::max(score_v, score_gap_row_v);
-			score_gap_row_v = (vec::max(score_v - giv, score_gap_row_v) - gev)<<1;
-			score_v = vec::max(score_v, score_gap_row_v);
-			score_gap_row_v = (vec::max(score_v - giv, score_gap_row_v) - gev2)<<2;
-			score_v = vec::max(score_v, score_gap_row_v);
-			score_gap_row_v = (vec::max(score_v - giv, score_gap_row_v) - gev4)<<4;
-			score_v = vec::max(score_v, score_gap_row_v);
+				pv.print("pv (tail)");
+				pf.print("pf (tail)");
 
-			/* xdrop test */
-			if((best_score_v - score_v > xtv) == 0xffff) {
-				vec const xxv(-sc_min);
-				xxv.store(&ptr[a_index].best);
-				xxv.store(&ptr[a_index].best_gap);
-				if(a_index == first_a_index) {
-					next_score_v = score_v;
-					first_a_index++;
-				}
-			} else {
-				last_a_index = a_index;
-				score_v.store(&ptr[a_index].best);
-				score_gap_col_v.store(&ptr[a_index].best_gap);
-				best_temp_v = vec::max(best_temp_v, score_v);
+				pv.store(ptr[last_b_index].s);
+				zv.store(ptr[last_b_index].e);
+				pf.store(ptr[last_b_index].f);
+				last_b_index++;
+
+				pv -= gev8;
+				pf -= gev8;
 			}
-
-			prev_pv = pv;
-			prev_av = av;
 		}
 
-		debug("xdrop, first_a_index(%llu), last_a_index(%llu), a_size(%llu)", first_a_index, last_a_index, a_size);
-		if(first_a_index == a_size) { break; }
-		if(last_a_index < a_size - 1) {
-			a_size = last_a_index + 1;
-		} else {
-			score_v.print("pv (raw)"); score_gap_row_v.print("gap_v (raw)");
-			while((best_score_v - score_v <= xtv) && a_size <= roundup(alen + 1, vec::LEN) / vec::LEN) {
-				score_v = (vec::max(score_v - giv, score_gap_row_v) - gev)>>7;
-				score_v.set(score_v[0]);
-				score_v -= acc_gev;
-				score_gap_row_v = score_v - giv;
-				score_v.store(&ptr[a_size].best);
-				score_gap_row_v.store(&ptr[a_size].best_gap);
-				a_size++;
-			}
-			// printf("a(%d), b(%d)\n", a_size, b_index);
-			last_a_index = a_size;
-		}
-/*
-		if(a_size <= alen) {
-			zv.store(ptr[a_size].best);
-			zv.store(ptr[a_size].best_gap);
-			a_size += vec::LEN;
-		}
-*/
-		best_score_v.set(best_temp_v.hmax());
+		max.set(mv.hmax());
 	}
-	// free(mat);
 
-	return(best_score_v.hmax() - OFS);
+	maxpos_t *r = (maxpos_t *)work;
+	r->alen = alen;
+	r->blen = blen;
+	// r->apos = amax;
+	// r->bpos = bmax;
+
+	return(max[0] - OFS);
 }
 
 #ifdef MAIN
